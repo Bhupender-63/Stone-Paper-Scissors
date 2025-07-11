16911bdb1bf16f957dbbca5af8884aa94c48d56f
Bhupender-63 <bhupenderbalwada07@gmail.com>
2025-07-11 11:26:44 +0530

    Added Stone paper scissors game

---
+++ src/Stone_paper_scissor.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Stone_paper_scissor {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        //Display choice
+        System.out.println("Welcome to stone paper scissors");
+        System.out.println("Enter your choice:0 for stone,1 for paper,2 for scissors!");
+
+        //Get user choice
+        int userChoice = sc.nextInt();
+        //Generate computer choice
+        int computerChoice = random.nextInt(3);
+
+        //Show both choices
+        System.out.println("you choose:" + userChoice);
+        System.out.println("computer choose:" + computerChoice);
+
+        //Determine winner
+        if(userChoice == computerChoice){
+            System.out.println("draw the game");
+        } else if ((userChoice == 0 && computerChoice == 2)||(userChoice == 1 && computerChoice == 0)||(userChoice == 2 && computerChoice == 1)) {
+            System.out.println("You win");
+        }
+        else{
+            System.out.println("Computer wins!");
+        }
+         int choice=1;
+            switch (choice) {
+                case 0 -> System.out.println("stone");
+                case 1 -> System.out.println("paper");
+                case 2 -> System.out.println("scissor");
+                default -> System.out.println("invalid choice");
+            }
+
+    }
+}
